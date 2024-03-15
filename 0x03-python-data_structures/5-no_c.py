600898c44a4b506d2963ee3ba49d8504fdce4569
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-03-15 16:44:54 +0200

    edit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for idx in range(len(my_string)):
+        if my_string[idx] != "c" and my_string[idx] != "C":
+            new_string = new_string + my_string[idx]
+    return new_string
