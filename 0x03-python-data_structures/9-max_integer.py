a787c029f7e78c839964897cfd5df3780db72b4f
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-03-16 11:33:43 +0200

    edit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = 0
+    for element in my_list:
+        if max < element:
+            max = element
+    return max
