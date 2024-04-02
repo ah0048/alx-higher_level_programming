dc08996e6170d0bb10bbd599cca2a433daaec682
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-04-02 16:20:54 +0200

    edit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             c += 1
         except (TypeError, ValueError):
             pass
+        except IndexError:
+            None
         i += 1
     print()
     return c
