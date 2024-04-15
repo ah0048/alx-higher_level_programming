204f58b2d013986eba908af57ffc51a1e6a09cd7
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-16 01:46:48 +0200

    edit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''module for BaseGeometry class'''
+
+
+class BaseGeometry:
+    '''class base geometry'''
+    def area(self):
+        raise Exception("area() is not implemented")
