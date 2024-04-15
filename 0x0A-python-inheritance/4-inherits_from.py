56a1db25f44ad7e5ce33de78a0cd9ebc9306e3f4
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-16 01:40:28 +0200

    edit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''checks if the object is an instance of a subclass'''
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return False if type(obj) is a_class else isinstance(obj, a_class)
