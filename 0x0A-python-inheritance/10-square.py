f03afe61b0284bdb7d3cf9843e8ef697fed81cdc
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-16 03:12:39 +0200

    edit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''module for square class'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''square class'''
+    def __init__(self, size):
+        '''constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    
+    def area(self):
+        return self.__size ^ 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''module for BaseGeometry class'''
+'''module for Rectangle class'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
