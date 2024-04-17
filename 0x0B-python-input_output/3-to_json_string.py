5d4422d3c7a57b182fc901d3961bf85313519e03
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 02:39:07 +0200

    edit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''module for append function'''
+import json
+
+
+def to_json_string(my_obj):
+    '''function to returns the JSON representation'''
+    return json.dumps(my_obj)
