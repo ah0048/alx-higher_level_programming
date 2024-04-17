0b3eca93dad43b37318ec09a1ef3693d1ee48e1d
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 16:54:36 +0200

    edit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''module for student class'''
+
+
+class Student:
+    '''defines a student'''
+
+    def __init__(self, first_name, last_name, age):
+        '''constructor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            dictionary = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    dictionary.update({attr: getattr(self, attr)})
+            return dictionary
+
+    def reload_from_json(self, json):
+        '''that replaces all attributes of the Student instance'''
+        for key, value in json.items():
+            if hasattr(json[key]):
+                setattr(self, key, value)
