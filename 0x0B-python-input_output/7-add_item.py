2fca79e9f952a1c246e8d30c5e5e1a68018e8743
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 04:42:22 +0200

    edit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,9 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-data = load_from_json_file('add_item.json')
+try:
+    data = load_from_json_file('add_item.json')
+except Exception:
+    data = []
 data.extend(argv[1:])
 save_to_json_file(data, 'add_item.json')
