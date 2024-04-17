d447c1f6ede5e9a3eda9972bb7214dd8aa88d5c9
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 18:15:43 +0200

    edit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,7 +3,7 @@
 
 
 def pascal_triangle(n):
-    '''returns a list of lists of integers representing the Pascal’s triangle of n'''
+    '''returns a matrix of Pascal’s triangle of n'''
     if n <= 0:
         return []
     matrix = []
