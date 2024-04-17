b6b14132a610b19090fdb5c0c797ba8c521823cb
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 02:59:23 +0200

    edit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     ''' creates an Object from a "JSON file"'''
     with open(filename, 'r', encoding="utf-8") as file:
-        return json.load(filename)
+        return json.load(file)
