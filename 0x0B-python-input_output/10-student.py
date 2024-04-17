67ee3b18d09154f464b31c3001fb1cac650b4905
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-17 16:05:21 +0200

    edit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
 
     def to_json(self, attrs=None):
         '''retrieves a dictionary representation'''
-        if not attrs or attrs == []:
+        if attrs is None:
             return self.__dict__
         else:
             dictionary = {}
