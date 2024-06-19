557a8c0abbc285a9c93856f9e3eab44e8e8ef06b
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-06-19 04:35:32 +0300

    edit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const arrayStatments = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arrayStatments.length; i++) {
+  console.log(arrayStatments[i]);
+}
