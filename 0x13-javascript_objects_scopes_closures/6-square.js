42ebc2df9c4ba29fc49d5025664eba068169d915
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-06-20 03:20:56 +0300

    edit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,11 @@ const SquareParent = require('./5-square');
 class Square extends SquareParent {
   charPrint (c) {
     if (c === undefined) {
-        c = 'X'
+      c = 'X';
     }
     for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.width));
-      }
+      console.log(c.repeat(this.width));
+    }
   }
 }
 module.exports = Square;
