ea8cfc0e6b24b43eb428265495d776d31ba660f4
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-06 16:02:49 +0300

    edit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''takes in a URL and an email, sends a POST request to the passed URL'''
+from urllib import request
+from urllib import parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+    encoded_data = parse.urlencode(data).encode('utf-8')
+    req = request.Request(url, data=encoded_data, method='POST')
+    with request.urlopen(url) as resp:
+        body = resp.read().encode('utf-8')
+        print(body)
