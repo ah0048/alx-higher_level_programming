330470dd335b813d90a2327361e991eb77c0ebc3
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-07 06:22:45 +0300

    edit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
     resp = requests.get(url)
     json_data = resp.json()
-    for i in range(0, 10):
-        print("{}: {}".format(json_data[i]['sha'],
-                              json_data[i]['commit']['author']['name']))
+    try:
+        for i in range(0, 10):
+            print("{}: {}".format(json_data[i]['sha'],
+                                  json_data[i]['commit']['author']['name']))
+    except IndexError:
+        pass
