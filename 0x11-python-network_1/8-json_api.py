897eff7ba15bedfc460c9e3a505f649474233660
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-07 04:33:57 +0300

    edit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''takes in a letter and sends a POST request to url'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if argv[1]:
+        q = argv[1]
+    else:
+        q = ""
+    data = {'q': q}
+    resp = requests.post(url, data=q)
+
+    try:
+        json_data = resp.json()
+        if not json_data:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+    except ValueError:
+        print('Not a valid JSON')
