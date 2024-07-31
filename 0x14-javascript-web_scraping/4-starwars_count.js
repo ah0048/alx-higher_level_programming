e8d38f6c2c419d4ba18c9a0b563debf6777b203a
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-31 12:44:03 +0300

    edit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const UrlFilms = process.argv[2];
-const CharacterWanted = 'https://swapi-api.alx-tools.com/api/people/18/';
 request.get(UrlFilms, (err, resp, body) => {
-  let NumFilms = 0;
   if (err) {
     console.error(err);
   } else {
-    body = JSON.parse(body);
-    for (const result of body.results) {
-      for (const character of result.characters) {
-        if (CharacterWanted === character) {
-          NumFilms += 1;
-        }
-      }
-    }
-    console.log(NumFilms);
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, film) => {
+      return film.characters.find((character) => character.endsWith('/18/')) ? count + 1 : count;
+    }, 0));
   }
 });
