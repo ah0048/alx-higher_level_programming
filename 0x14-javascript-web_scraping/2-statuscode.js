0f6f9cb182702bff5f8c74f277fc18afc277e327
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-31 10:22:58 +0300

    edit

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (err, resp, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code:', resp.statusCode);
+  }
+});
