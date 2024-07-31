d5af1918a010123e71652d3f6e1932d1d8e4dfbd
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-31 10:40:41 +0300

    edit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request.get(url, (err, resp, body) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(JSON.parse(body)["title"]);
-    }
-  });
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
