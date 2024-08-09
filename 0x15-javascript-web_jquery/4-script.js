c589f483e3838523ad60eff642e88176f40d6d1f
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-09 17:26:37 +0300

    edit

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$('DIV#toggle_header').on('click', function () {
+    if ($('header').hasClass('red') && $('header').hasClass('green')) {
+        $('header').removeClass('green');
+    }
+    else if ($('header').hasClass('red')) {
+        $('header').removeClass('red');
+        $('header').addClass('green');
+    }
+    else {
+        $('header').removeClass('green');
+        $('header').addClass('red');
+    }
+  });
